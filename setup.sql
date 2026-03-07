5c21c22f8fd1857986ff8b242719ee2d802b2145
angshen <angshen@student.ubc.ca>
2026-03-06 20:34:12 -0800

    moved changes to setup.sql

--- setup.sql
+++ setup.sql
@@ -45,6 +45,43 @@ DROP TABLE Calibrated_Definition CASCADE CONSTRAINTS;
 DROP TABLE Calibrated_User CASCADE CONSTRAINTS;
 
 /* create tables */
+CREATE TABLE Contained_Analyzed_Frame2( 
+    data VARCHAR[255] PRIMARY KEY, 
+    resolution VARCHAR[255] NOT NULL
+) ;
+
+CREATE TABLE Translated_Word_7 ( 
+    Translation VARCHAR[255] PRIMARY KEY, 
+    character_length INT);
+
+     
+CREATE TABLE Predicted_Gesture_Handmark2( 
+    def_id INT PRIMARY KEY, 
+    number_of_frames INT NOT NULL, 
+    x_position VARCHAR[255] NOT NULL, 
+    y_position VARCHAR[255] NOT NULL, 
+    UNIQUE(x_position, y_position));
+
+    
+CREATE TABLE Trained_Machine_Learning_Model( 
+    model_id INT PRIMARY KEY, 
+    handmark_id INT, 
+    accuracy INT, 
+    hyperparameter INT NOT NULL, 
+    model_type VARCHAR[255]);
+
+CREATE TABLE Predicted_Gesture_Handmark1( 
+    handmark_id INT PRIMARY KEY, 
+    def_id INT, 
+    model_id INT, 
+    FOREIGN KEY (model_id) REFERENCES Trained_Machine_Learning_Model (model_id) 
+    ON DELETE CASCADE, 
+    FOREIGN KEY (def_id) REFERENCES Predicted_Gesture_Handmark2(def_id) 
+    ON DELETE CASCADE);
+
+ALTER TABLE Trained_Machine_Learning_Model
+    ADD CONSTRAINT fk_mlmodel_handmark
+    FOREIGN KEY (handmark_id) REFERENCES Predicted_Gesture_Handmark1 (handmark_id);
 
 CREATE TABLE Calibrated_User( 
     user_id INT PRIMARY KEY, 
@@ -66,52 +103,15 @@ CREATE TABLE Calibrated_Definition(
     ON DELETE CASCADE
 ); 
 
-CREATE TABLE Created_Documented_Recording( 
+ALTER TABLE Calibrated_User
+    ADD CONSTRAINT fk_user_def
+    FOREIGN KEY (def_id) REFERENCES Calibrated_Definition (def_id);
+
+CREATE TABLE Documented_Saved_Transcript_7 ( 
     recording_id INT PRIMARY KEY, 
     transcript_id INT, 
-    user_id INT,  
-    fps INT NOT NULL, 
-    recording_date DATE NOT NULL,  
-    recording_name VARCHAR[50] NOT NULL, 
-    FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1(transcript_id), 
-    FOREIGN KEY (user_id) REFERENCES Calibrated_User (user_id) 
-    ON DELETE CASCADE, 
-    UNIQUE(recording_name)
-); 
-
-CREATE TABLE Contained_Analyzed_Frame1( 
-    data VARCHAR[255] NOT NULL, 
-    recording_id INT, 
-    frame_id INT, 
-    timestamp TIMESTAMP NOT NULL, 
-    PRIMARY KEY(recording_id, frame_id), 
-    FOREIGN KEY(recording_id) REFERENCES  
-    Created_Documented_Recording(recording_id) 
-    ON DELETE CASCADE, 
-    FOREIGN KEY(data) REFERENCES Contained_Analyzed_Frame2(data) 
-    ON DELETE CASCADE
-);
-
-CREATE TABLE Contained_Analyzed_Frame2( 
-    data VARCHAR[255] PRIMARY KEY, 
-    resolution VARCHAR[255] NOT NULL
-) ;
-
-
-CREATE TABLE Live( 
-    recording_id INT PRIMARY KEY, 
-    livestream_source VARCHAR[255] NOT NULL, 
-    FOREIGN KEY (recording_id) REFERENCES Created_Documented_Recording(recording_id) 
-    ON DELETE CASCADE
-);
-
-CREATE TABLE Video( 
-    recording_id INT PRIMARY KEY, 
-    duration INT NOT NULL, 
-    FOREIGN KEY (recording_id) REFERENCES Created_Documented_Recording (recording_id) 
-    ON DELETE CASCADE
-);
- 
+    FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1 (transcript_id) 
+    ON DELETE CASCADE);
 
 CREATE TABLE Documented_Saved_Transcript_1 (
     transcript_id INT PRIMARY KEY, 
@@ -119,6 +119,11 @@ CREATE TABLE Documented_Saved_Transcript_1 (
     FOREIGN KEY (recording_id) REFERENCES Documented_Saved_Transcript_7 (recording_id) 
     ON DELETE CASCADE
 );
+
+ALTER TABLE Documented_Saved_Transcript_7
+    ADD CONSTRAINT fk_t7_transcript
+    FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1 (transcript_id)
+        ON DELETE CASCADE;
 
 CREATE TABLE Documented_Saved_Transcript_2 ( 
     transcript_id INT PRIMARY KEY, 
@@ -156,12 +161,6 @@ CREATE TABLE Documented_Saved_Transcript_6 (
     FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1 (transcript_id) 
     ON DELETE CASCADE);
 
-CREATE TABLE Documented_Saved_Transcript_7 ( 
-    recording_id INT PRIMARY KEY, 
-    transcript_id INT, 
-    FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1 (transcript_id) 
-    ON DELETE CASCADE);
-
 
 CREATE TABLE Documented_Saved_Transcript_8 ( 
     recording_id INT PRIMARY KEY, 
@@ -175,8 +174,6 @@ CREATE TABLE Documented_Saved_Transcript_9 (
     recording_id INT PRIMARY KEY, 
     transcript_data VARCHAR[255], 
     FOREIGN KEY (recording_id) REFERENCES Documented_Saved_Transcript_7 (recording_id) 
-    ON DELETE CASCADE, 
-    FOREIGN KEY (transcript_data) REFERENCES Documented_Saved_Transcript_10 (transcript_data) 
     ON DELETE CASCADE);
 
 CREATE TABLE Documented_Saved_Transcript_10 ( 
@@ -189,8 +186,6 @@ CREATE TABLE Documented_Saved_Transcript_11 (
     recording_id INT PRIMARY KEY, 
     transcription_date date, 
     FOREIGN KEY (recording_id) REFERENCES Documented_Saved_Transcript_7 (recording_id) 
-    ON DELETE CASCADE, 
-    FOREIGN KEY (transcript_data) REFERENCES Documented_Saved_Transcript_10 (transcript_data) 
     ON DELETE CASCADE);
 
 CREATE TABLE Documented_Saved_Transcript_12 ( 
@@ -204,12 +199,52 @@ CREATE TABLE Documented_Saved_Transcript_13 (
     word_count INT, 
     FOREIGN KEY (transcript_data) REFERENCES Documented_Saved_Transcript_10 (transcript_data) 
     ON DELETE CASCADE);
-
+    
 CREATE TABLE Documented_Saved_Transcript_14 ( 
     transcript_data VARCHAR[255] PRIMARY KEY, 
     language VARCHAR[50], 
-    FOREIGN KEY (	transcript_data) REFERENCES Documented_Saved_Transcript1 (transcript_id) 
+    FOREIGN KEY (transcript_data) REFERENCES Documented_Saved_Transcript1 (transcript_id) 
     ON DELETE CASCADE);
+
+CREATE TABLE Created_Documented_Recording( 
+    recording_id INT PRIMARY KEY, 
+    transcript_id INT, 
+    user_id INT,  
+    fps INT NOT NULL, 
+    recording_date DATE NOT NULL,  
+    recording_name VARCHAR[50] NOT NULL, 
+    FOREIGN KEY (transcript_id) REFERENCES Documented_Saved_Transcript_1(transcript_id), 
+    FOREIGN KEY (user_id) REFERENCES Calibrated_User (user_id) 
+    ON DELETE CASCADE, 
+    UNIQUE(recording_name)
+); 
+
+CREATE TABLE Live( 
+    recording_id INT PRIMARY KEY, 
+    livestream_source VARCHAR[255] NOT NULL, 
+    FOREIGN KEY (recording_id) REFERENCES Created_Documented_Recording(recording_id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Video( 
+    recording_id INT PRIMARY KEY, 
+    duration INT NOT NULL, 
+    FOREIGN KEY (recording_id) REFERENCES Created_Documented_Recording (recording_id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Contained_Analyzed_Frame1( 
+    data VARCHAR[255] NOT NULL, 
+    recording_id INT, 
+    frame_id INT, 
+    timestamp TIMESTAMP NOT NULL, 
+    PRIMARY KEY(recording_id, frame_id), 
+    FOREIGN KEY(recording_id) REFERENCES  
+    Created_Documented_Recording(recording_id) 
+    ON DELETE CASCADE, 
+    FOREIGN KEY(data) REFERENCES Contained_Analyzed_Frame2(data) 
+    ON DELETE CASCADE
+);
 
 CREATE TABLE Translated_Word_1 ( 
     instance_id INT PRIMARY KEY,  
@@ -220,11 +255,7 @@ CREATE TABLE Translated_Word_1 (
 CREATE TABLE Translated_Word_2 ( 
     instance_id INT PRIMARY KEY,  
     handmark_id INT, 
-<<<<<<< HEAD:data.sql
     FOREIGN KEY (handmark_id) REFERENCES Predicted_Gesture_Handmark1(handmark_id)  
-=======
-    FOREIGN KEY (handmark_id) REFERENCES Predicted_Gesture_Handmark1 (handmark_id)  
->>>>>>> 19484daa16a8a5530450d8ea7b5f58886fbb4d53:setup.sql
     ON DELETE CASCADE, 
     FOREIGN KEY (instance_id) REFERENCES Translated_Word_1 (instance_id) 
     ON DELETE CASCADE);
@@ -258,10 +289,6 @@ CREATE TABLE Translated_Word_6 (
     FOREIGN KEY (instance_id) REFERENCES Translated_Word_1 (instance_id) 
     ON DELETE CASCADE);
 
-CREATE TABLE Translated_Word_7 ( 
-    Translation VARCHAR[255] PRIMARY KEY, 
-    character_length INT);
-
 CREATE TABLE Translated_Word_8 ( 
     model_id INT, 
     handmark_id INT, 
@@ -280,27 +307,3 @@ CREATE TABLE Translated_Word_9 (
     word_timestamp TIMESTAMP NOT NULL, 
     FOREIGN KEY (handmark_id) REFERENCES Predicted_Gesture_Handmark1(handmark_id) 
     ON DELETE CASCADE);
-
-CREATE TABLE Predicted_Gesture_Handmark1( 
-    handmark_id INT PRIMARY KEY, 
-    def_id INT, 
-    model_id INT, 
-    FOREIGN KEY (model_id) REFERENCES Trained_Machine_Learning_Model (model_id) 
-    ON DELETE CASCADE, 
-    FOREIGN KEY (def_id) REFERENCES Predicted_Gesture_Handmark2(def_id) 
-    ON DELETE CASCADE);
- 
-CREATE TABLE Predicted_Gesture_Handmark2( 
-    def_id INT PRIMARY KEY, 
-    number_of_frames INT NOT NULL, 
-    x_position VARCHAR[255] NOT NULL, 
-    y_position VARCHAR[255] NOT NULL, 
-    UNIQUE(x_position, y_position));
-
-CREATE TABLE Trained_Machine_Learning_Model( 
-    model_id INT PRIMARY KEY, 
-    handmark_id INT, 
-    accuracy INT, 
-    hyperparameter INT NOT NULL, 
-    model_type VARCHAR[255], 
-    FOREIGN KEY (handmark_id) REFERENCES Predicted_Gesture_Handmark (handmark_id));
